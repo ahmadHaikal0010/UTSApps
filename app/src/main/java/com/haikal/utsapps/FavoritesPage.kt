552e4aa1f478f776c190c356d70da73617930dfa
Ahmad Haikal <haikala0010@gmail.com>
2024-10-23 13:06:18 +0700

    Create Foods Apps

---
+++ app/src/main/java/com/haikal/utsapps/FavoritesPage.kt
@@ -0,0 +1,37 @@
+package com.haikal.utsapps
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.haikal.utsapps.adapter.FavoritesAdapter
+import com.haikal.utsapps.model.FavoritesList
+import com.haikal.utsapps.model.ModelFavorites
+
+class FavoritesPage : AppCompatActivity() {
+
+    private lateinit var rv_favorites : RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_favorites_page)
+
+        rv_favorites = findViewById(R.id.rv_favorites)
+
+        rv_favorites.layoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+
+        val adapter = FavoritesAdapter(FavoritesList.getModel() as ArrayList<ModelFavorites>, this)
+
+        rv_favorites.adapter = adapter
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/haikal/utsapps/adapter/FavoritesAdapter.kt
@@ -0,0 +1,57 @@
+package com.haikal.utsapps.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.haikal.utsapps.DetailOrderPage
+import com.haikal.utsapps.FavoritesPage
+import com.haikal.utsapps.R
+import com.haikal.utsapps.model.ModelFavorites
+
+class FavoritesAdapter(
+    val itemList : ArrayList<ModelFavorites>,
+    val getActivity : FavoritesPage
+) : RecyclerView.Adapter<FavoritesAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var gambarMakanan : ImageView
+        var namaMakanan : TextView
+        var detailMakanan : TextView
+
+        init {
+            gambarMakanan = itemView.findViewById(R.id.gambarMakanan) as ImageView
+            namaMakanan = itemView.findViewById(R.id.namaMakanan) as TextView
+            detailMakanan = itemView.findViewById(R.id.detailMakanan) as TextView
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val nView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_favorites, parent, false)
+
+        return MyViewHolder(nView)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.gambarMakanan.setImageResource(itemList[position].gambarMakanan)
+        holder.namaMakanan.setText(itemList[position].namaMakanan)
+        holder.detailMakanan.setText(itemList[position].detailMakanan)
+
+        holder.itemView.setOnClickListener() {
+            val intent = Intent(getActivity, DetailOrderPage::class.java)
+            intent.putExtra("gambarMakanan", itemList[position].gambarMakanan)
+            intent.putExtra("namaMakanan", itemList[position].namaMakanan)
+
+            getActivity.startActivity(intent)
+        }
+    }
+
+}
